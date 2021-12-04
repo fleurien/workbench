8bae4ac75acbb2cbae8156a317e117b5470988a9
Fleurien <flgraveson@gmail.com>
2021-12-04 15:15:40 -0500

    Create letMasterIn.js

---
+++ letMasterIn.js
@@ -0,0 +1,12 @@
+let user = prompt("Who's there?","");
+if(user=="Admin"){
+    let password = prompt("Welcome! What's the password?");
+        if(password=="open"){
+            alert("Welcome back, master!")} 
+            else{
+                alert("YOU ARE NOT MASTER!")
+            }
+        }
+    else{
+        alert("I don't know you.")
+    }
